73ceae71498535d64780c2503d7d1a313fa3ad22
DESKTOP-IRT3FAR\lephu <lephuctri19922@gmail.com>
2023-06-12 12:47:53 +0700

    init 2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://abc.com/',
+  base: 'https://symphonious-cupcake-f33bfb.netlify.app',
 })
