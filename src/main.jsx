e63e0d9131c76bca815e1f441c18916a3ddb70b0
DESKTOP-IRT3FAR\lephu <lephuctri19922@gmail.com>
2023-06-12 20:32:47 +0700

    init 5

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-
+import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/product",
+    element: 'Product Page',
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
